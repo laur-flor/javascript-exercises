9fae10c5cbc71bc2e637ef767912ba1dc7b9cde7
laur <laura.r.brackney@gmail.com>
2024-02-23 15:41:51 -0600

    Complete repeating test exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = '';
+    if (num >= 0) {
+        for (let i = 1; i <=num; i++) {
+        result += string;
+        }
+        return result;
+    } else {
+        return 'ERROR';
+    }
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
