3f7c73e7a98339ab7282fd99d120cd31aa77e315
laur <laura.r.brackney@gmail.com>
2024-02-27 18:31:50 -0600

    Move console log statement

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
 
 const removeFromArray = function(array, ...args) {
-    for (let i = array.length; i >=0; i--) {
+    for (let i = (array.length - 1); i >=0; i--) {
         if (args.includes(array[i])) {
             array.splice(i, 1);
         }
