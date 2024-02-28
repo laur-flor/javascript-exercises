f67ae2fcf27add2ad7e13f6e87e3c7655d8e0bd0
laur <laura.r.brackney@gmail.com>
2024-02-28 15:07:05 -0600

    Practice rounding with Math function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let exactCelsius;
+  let roundCelsius;
+  exactCelsius = (fahrenheit - 32) * 5/9;
+  roundCelsius = Math.round(exactCelsius * 10) / 10;
+  return roundCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let exactFahrenheit;
+  let roundFahrenheit;
+  exactFahrenheit = (celcius * (9/5)) + 32;
+  roundFahrenheit = Math.round(exactFahrenheit * 10) / 10;
+  return roundFahrenheit;
 };
 
 // Do not edit below this line
