a88f243895db8b9e5c542366fdc4882ef20bc69f
tamlfrisic <tamlfrisic@gmail.com>
2024-06-05 18:45:57 -0500

    Complete fibonacci with for loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    const series = [0, 1];
+    if (n < 0) {
+        return "OOPS";
+    } else {
+        for (let i = 2; i <= n; i++) {
+            series[i] = series[i-1] + series[i-2];
+            console.log(series[n]);
+        }
+        return series[n];
+    }
 };
+
+
+  
+  fibonacci(8);
 
 // Do not edit below this line
 module.exports = fibonacci;
