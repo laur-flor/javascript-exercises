b3e33e3177c65478a9b3087a2f807d0829256305
tamlfrisic <tamlfrisic@gmail.com>
2024-06-06 00:57:12 -0500

    Remove unwanted characters and spaces

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+// filter to compare if firstIndex and lastIndex are equal to each other 
 
+const palindromes = function (str) {
+    let cleanStr = str.replace(/[|&;$!%@"<>()+,.]/g, "").toLowerCase();
+    cleanStr = cleanStr.replaceAll(" ","");
+    let reverse = cleanStr.split("").reverse().join("");
+    return cleanStr === reverse;
 };
 
 // Do not edit below this line
