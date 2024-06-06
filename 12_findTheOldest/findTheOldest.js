24c28f884ffc66f7233cf6155c226fb445dc5a2d
tamlfrisic <tamlfrisic@gmail.com>
2024-06-06 00:29:19 -0500

    Complete with toSorted

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const assignAge = function(person) {
+    let age = "";
+    if (person.yearOfDeath === undefined) {
+      age = ((new Date()).getFullYear()) - person.yearOfBirth;
+    } else {
+      age = person.yearOfDeath - person.yearOfBirth;
+    }
+    console.log("ages: " + person.name + "=" + age);
+    return age;
+  }
+  
+// console.log(assignDeath(arr)); // works!
 
+const findTheOldest = function(arr) {
+  const oldestArray = arr.toSorted((a, b) => (assignAge(a) > assignAge(b)) ? -1 : 1);
+  const oldest = oldestArray[0];
+  // console.log(oldestArray);
+  console.log(oldest);
+  return oldest;
 };
 
 // Do not edit below this line
